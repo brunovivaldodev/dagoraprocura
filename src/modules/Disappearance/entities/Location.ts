293dc9907ae66cad2c800192ac1fe565608c1875
Bruno Vivaldo <brunolumeca@live.com>
2021-10-04 13:48:41 +0100

    Location Timestamps added

--- src/modules/Disappearance/entities/Location.ts
+++ src/modules/Disappearance/entities/Location.ts
@@ -2,14 +2,18 @@ import { UuidProvider } from "../../../shared/providers/uuid/implemnetations/Uui
 import { Provinces } from "./Provinces"
 
 class Location {
-    id: string
-    district: string
-    province: string
+    private id: string
+    private district: string
+    private province: string
+    private created_at : string
+    private updated_at : string
 
     constructor(district: string, province: Provinces) {
         this.id = UuidProvider.uuid()
         this.district = district,
-        this.province = province
+        this.province = province,
+        this.created_at = new Date().toISOString(),
+        this.updated_at = new Date().toISOString()
     }
 }
 
